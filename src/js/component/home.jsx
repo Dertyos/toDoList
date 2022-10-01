5a6aba68b73f9878f4bbbfc3640c4a835c1c8579
Dertyos <juliansalcedo4@gmail.com>
2022-10-01 02:42:33 +0000

    first commit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
-
+import { ToDo } from "./todo.jsx";
 //create your first component
 const Home = () => {
 	return (
---
+++ src/js/component/todo.jsx
@@ -0,0 +1,5 @@
+import React,{useState} from "react";
+
+export const ToDo = () => {
+    const[tarea,setState] = useState()
+}
