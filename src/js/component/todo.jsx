3b521f82afe314c8ac6ede96b147af1b998296a0
Dertyos <juliansalcedo4@gmail.com>
2022-10-01 03:56:04 +0000

    Terminado, faltan bonus

--- src/js/component/todo.jsx
+++ src/js/component/todo.jsx
@@ -1,5 +1,45 @@
 import React,{useState} from "react";
 
 export const ToDo = () => {
-    const[tarea,setState] = useState()
+    const[listToDo,setToDo] = useState([])
+
+const deleteItem = (indiceTarea) => {
+    setToDo((prevState)=>{
+        return prevState.filter((item,index) => {
+            return index !== indiceTarea;
+        })
+    })
+}
+return(
+    <div className="card m-3">
+        <input type="text" placeholder="Escribe una nueva tarea"
+        onKeyUp={(e)=>{
+            if(e.keyCode == "13") {
+                let arrayAux = listToDo.slice()
+                arrayAux.push(e.target.value)
+                setToDo(arrayAux)
+                e.target.value = ""
+            }
+        }}
+        />
+        <ul>
+            {listToDo.map((item,indice) => {
+                return(
+                    <li className="m-2 list-group-item d-flex justify-content-between"
+                    key={indice}>
+                        {item}
+                    <button type="button"                     
+                    className="btn btn-light"
+                    onClick={(e)=>{
+                        deleteItem(indice)
+                    }}>x</button>
+                    </li>
+                )
+            })
+            }
+        </ul>
+        
+    </div>
+)
+
 }
